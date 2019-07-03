1b3f62f625e1597d932c5ff4427df1040bfc779d
fakefrog <qwe6588038>
2019-07-04 01:14:47 +0800

    init

--- algorithm/src/main/java/com/twodfire/algorithm/sort/MergeSort.java
+++ algorithm/src/main/java/com/twodfire/algorithm/sort/MergeSort.java
@@ -1,5 +1,8 @@
 package com.twodfire.algorithm.sort;
 
+import com.sun.deploy.uitoolkit.impl.text.TextWindow;
+import com.sun.media.sound.SoftMixingDataLine;
+
 import java.util.Arrays;
 
 /**
@@ -16,6 +19,33 @@ public class MergeSort {
     }
 
     public static void mergerSort(int[] nums, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+        int middle = (start + end) / 2;
+        mergerSort(nums, start, middle);
+        mergerSort(nums, middle + 1, end);
+        //start -> middle   middle+1->end
+        merge(nums, start, middle, end);
+    }
+
+    private static void merge(int[] nums, int start, int middle, int end) {
+        int[] temp = new int[end - start + 1];
+        int index = 0;
+        int left = start;
+        int right = middle + 1;
+        while (left <= middle && right <= end) {
+            temp[index++] = nums[left] < nums[right] ? nums[left++] : nums[right++];
+        }
+        while (left <= middle) {
+            temp[index++] = nums[left++];
+        }
+        while (right <= end) {
+            temp[index++] = nums[right++];
+        }
+        for (int i = 0; i < temp.length; i++) {
+            nums[start + i] = temp[i];
+        }
     }
 
 }
